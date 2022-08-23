c27642c346b0da1a9a876bcc1364c9265e052482
Arthur Lage <arthurlage2006@gmail.com>
2022-08-23 17:14:40 -0300

    final project

---
+++ README.md
@@ -0,0 +1,50 @@
+# League Client
+
+## ✉ About
+
+A clone of the League of Legends game client, made using modern web technologies.
+
+## 🌆 Images
+
+
+
+## ⚠ Requirements
+
+In order to run the project, you will need:
+
+- Node
+- A web browser (Chrome, Firefox, Edge, etc...)
+
+## 💻 Technologies Used
+
+### FRONTEND
+
+- HTML
+- CSS
+- JavaScript
+- ReactJS
+- Typescript
+
+### EDITOR
+
+- Visual Studio Code
+
+## ⬇ How to download the project
+
+```bash
+
+git clone https://github.com/arthur-lage/league-client.git
+
+cd league-client
+
+npm install
+
+npm run dev
+
+```
+
+## 🔗 How to use the app
+
+If you wish to test the application, click the link below:
+
+[League Client](https://league-client-al.vercel.com)
--- src/App.tsx
+++ src/App.tsx
@@ -33,100 +33,118 @@ export function App() {
   }
 
   return (
-    <div className="container">
-      <div className="left">
-        <img className="logo" src={RiotGamesLogo} alt="RIOT GAMES logo" />
+    <>
+      <div className="container">
+        <div className="left">
+          <img className="logo" src={RiotGamesLogo} alt="RIOT GAMES logo" />
 
-        <h1 className="title">Sign in</h1>
+          <h1 className="title">Sign in</h1>
 
-        <form onSubmit={handleForm}>
-          <div className="input-wrapper">
-            <div className="input-field">
-              <label htmlFor="username">Username</label>
-              <input
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                type="text"
-                id="username"
-              />
+          <form onSubmit={handleForm}>
+            <div className="input-wrapper">
+              <div className="input-field">
+                <input
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  type="text"
+                  placeholder=" "
+                  id="username"
+                  className="form__input"
+                />
+                <label className="form__label" htmlFor="username">
+                  Username
+                </label>
+              </div>
+              <div className="input-field">
+                <input
+                  value={password}
+                  placeholder=" "
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  id="password"
+                  className="form__input"
+                />
+                <label className="form__label" htmlFor="password">
+                  Password
+                </label>
+              </div>
             </div>
-            <div className="input-field">
-              <label htmlFor="password">Password</label>
-              <input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-                id="password"
-              />
+
+            <div className="social-login">
+              <button className="facebook">
+                <BsFacebook size={16} color="#fff" />
+              </button>
+              <button className="google">
+                <FcGoogle size={16} />
+              </button>
+              <button className="apple">
+                <AiFillApple color="#fff" size={16} />
+              </button>
+            </div>
+            <label className="stay-signed-in">
+              <span>Stay signed in</span>
+              <input type="checkbox" id="stay-signed-in" />
+              <span className="checkmark"></span>
+            </label>
+
+            <div className="submit-form-wrapper">
+              <button
+                className={`submit-form ${isFormValid ? "valid" : ""}`}
+                type="submit"
+              >
+                <BsArrowRight size={36} />
+              </button>
+            </div>
+          </form>
+
+          <div className="other-links">
+            <a className="cant-sign-in" href="#">
+              CAN'T SIGN IN?
+            </a>
+            <a className="create-account" href="#">
+              CREATE ACCOUNT
+            </a>
+
+            <a className="version" href="#">
+              V56.0.0
+            </a>
+          </div>
+        </div>
+
+        <div className="right">
+          <img
+            className="characters"
+            src={CharactersImage}
+            alt="Kai'sa, Jayce, Ornn, Taliyah, Pyke in battleI"
+          />
+
+          <div className="warning-wrapper">
+            <div className="warning">
+              <BsExclamation color="#111" size={40} />
             </div>
           </div>
 
-          <div className="social-login">
-            <button className="facebook">
-              <BsFacebook size={16} color="#fff" />
+          <button className="user-wrapper">
+            <BsPersonFill size={32} />
+          </button>
+
+          <div className="window-options">
+            <button className="minimize">
+              <BsDash size={20} />
             </button>
-            <button className="google">
-              <FcGoogle size={16} />
-            </button>
-            <button className="apple">
-              <AiFillApple color="#fff" size={16} />
+            <button className="close">
+              <BsX size={20} />
             </button>
           </div>
-          <div className="stay-signed-in">
-            <input type="checkbox" id="stay-signed-in" />
-            <label htmlFor="stay-signed-in">Stay signed in</label>
-          </div>
-
-          <div className="submit-form-wrapper">
-            <button
-              className={`submit-form ${isFormValid ? "valid" : ""}`}
-              type="submit"
-            >
-              <BsArrowRight size={36} />
-            </button>
-          </div>
-        </form>
-
-        <div className="other-links">
-          <a className="cant-sign-in" href="#">
-            CAN'T SIGN IN?
-          </a>
-          <a className="create-account" href="#">
-            CREATE ACCOUNT
-          </a>
-
-          <a className="version" href="#">
-            V56.0.0
-          </a>
         </div>
       </div>
-
-      <div className="right">
-        <img
-          className="characters"
-          src={CharactersImage}
-          alt="Kai'sa, Jayce, Ornn, Taliyah, Pyke in battleI"
-        />
-
-        <div className="warning-wrapper">
-          <div className="warning">
-            <BsExclamation color="#111" size={40} />
-          </div>
-        </div>
-
-        <button className="user-wrapper">
-          <BsPersonFill size={32} />
-        </button>
-
-        <div className="window-options">
-          <button className="minimize">
-            <BsDash size={20} />
-          </button>
-          <button className="close">
-            <BsX size={20} />
-          </button>
-        </div>
+      <div className="invalid-screen-size">
+        <h2>Sorry!</h2>
+        <p>
+          This website is not available to your screensize. Minimun screen
+          width: 550px
+        </p>
       </div>
-    </div>
+    </>
   );
 }
